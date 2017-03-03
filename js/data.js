7e7a90f2e767a181dede735b52b174e81a6cf29c
MU\Administrator <416018952@qq.com>
2017-03-03 22:07:47 +0800

    no message

---
+++ js/data.js
@@ -0,0 +1,230 @@
+﻿var louceng = [{
+        link: "https://www.baidu.com/",
+        img: "img/mainPic_08.jpg"
+    }, {
+        link: "https://www.baidu.com/",
+        img: "img/mainPic_09.jpg"
+    }, {
+        link: "https://www.baidu.com/",
+        img: "img/mainPic_10.jpg"
+    }, {
+        link: "https://www.baidu.com/",
+        img: "img/mainPic_11.jpg"
+    }, {
+        link: "https://www.baidu.com/",
+        img: "img/mainPic_12.jpg"
+    }],
+    floorData = [
+        [{
+            "link": "http://product.suning.com/0070135683/184002921.html",
+            "img": "http://image1.suning.cn/uimg/b2c/newcatentries/0070135683-000000000184002921_2_800x800.jpg?ver=2001",
+            "goodsName": "烟台长岛拉缸盐海参",
+            "price": "458",
+            "sale": "229"
+        }, {
+            "link": "http://product.suning.com/0070090774/188859924.html",
+            "img": "http://image3.suning.cn/uimg/b2c/newcatentries/0070090774-000000000188859924_2_800x800.jpg?ver=2003",
+            "goodsName": "蚌埠馆 银鱼干 野生淡干海产面条鱼海鲜干货小银鱼干80g",
+            "price": "68",
+            "sale": "29.9"
+        }, {
+            "link": "http://product.suning.com/0070106098/146181313.html",
+            "img": "http://image2.suning.cn/uimg/b2c/newcatentries/0070106098-000000000146181313_2_800x800.jpg",
+            "goodsName": "【买2减5元】米又野生淡干小银鱼山东特产银鱼干250g 袋装",
+            "price": "99",
+            "sale": "39.9"
+        }, {
+            "link": "http://product.suning.com/0070113549/142315551.html",
+            "img": "http://image5.suning.cn/uimg/b2c/newcatentries/0070113549-000000000142315551_2_800x800.jpg",
+            "goodsName": "清水牌凤尾鱼干 洪泽湖特产淡水毛刀鱼淡晒特色水产干货 250g",
+            "price": "54",
+            "sale": "54"
+        }, {
+            "link": "http://product.suning.com/0070130909/146493795.html",
+            "img": "http://image4.suning.cn/uimg/b2c/newcatentries/0070130909-000000000146493795_2_800x800.jpg?ver=2001",
+            "goodsName": " 华容馆 湖北特产鄂州武昌鱼红烧味豆豉味真空包装500g*4条礼盒 ",
+            "price": "168",
+            "sale": "118"
+        }, {
+            "link": "http://product.suning.com/0000000000/156571644.html",
+            "img": "http://image4.suning.cn/uimg/b2c/newcatentries/0000000000-000000000156571644_2_800x800.jpg?ver=2004",
+            "goodsName": "金樱花 豆豉鱼罐头120克/3组",
+            "price": "13.6",
+            "sale": "12.9"
+        }],
+        [{
+            "link": "http://product.suning.com/0070137508/168018255.html",
+            "img": "http://image2.suning.cn/uimg/b2c/newcatentries/0070137508-000000000168018255_2_800x800.jpg?ver=2002",
+            "goodsName": "西峡馆 QS认证香菇酱油490ml瓶装纯天然无添加调味酱油l",
+            "price": "9.9",
+            "sale": "19"
+        }, {
+            "link": "http://product.suning.com/0070128007/176267334.html",
+            "img": "http://image2.suning.cn/uimg/b2c/newcatentries/0070128007-000000000176267334_2_800x800.jpg",
+            "goodsName": "五农好辣椒酱黄豆酱礼盒装230g*2瓶佐餐拌饭辣椒酱下饭菜",
+            "price": "36.9",
+            "sale": "29.9"
+        }, {
+            "link": "http://product.suning.com/0000000000/104433528.html",
+            "img": "http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000104433528_2_800x800.jpg?ver=2002",
+            "goodsName": "海天酱油 金标生抽 1.28L",
+            "price": "13.5",
+            "sale": "10.5"
+        }, {
+            "link": "http://product.suning.com/0000000000/104433526.html",
+            "img": "http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000104433526_1_800x800.jpg",
+            "goodsName": "海天草菇老抽500MLml",
+            "price": "8",
+            "sale": "5.5"
+        }, {
+            "link": "http://product.suning.com/0000000000/124057228.html",
+            "img": "http://image4.suning.cn/uimg/b2c/newcatentries/0000000000-000000000124057228_2_800x800.jpg?ver=2003",
+            "goodsName": "海天450ml白米醋",
+            "price": "5.4",
+            "sale": "5.2"
+        }, {
+            "link": "http://product.suning.com/0000000000/104436132.html",
+            "img": "http://image3.suning.cn/uimg/b2c/newcatentries/0000000000-000000000104436132_1_800x800.jpg?ver=2000",
+            "goodsName": "海天美厨鸡精227G",
+            "price": "8",
+            "sale": "6.5"
+        }],
+        [{
+            "link": "http://product.suning.com/0000000000/129681927.html",
+            "img": "http://image3.suning.cn/uimg/b2c/newcatentries/0000000000-000000000129681927_2_800x800.jpg?ver=2010",
+            "goodsName": "长帝（Changdi）TRTF32 上下独立控温 家用烘焙 32L 电烤箱",
+            "price": "268",
+            "sale": "424"
+        }, {
+            "link": "http://product.suning.com/0000000000/104234630.html",
+            "img": "http://image5.suning.cn/uimg/b2c/newcatentries/0000000000-000000000104234630_2_800x800.jpg",
+            "goodsName": "苏泊尔易存储不锈钢复底蒸锅SZ28B2",
+            "price": "399",
+            "sale": "189"
+        }, {
+            "link": "http://product.suning.com/0000000000/140843141.html",
+            "img": "http://image3.suning.cn/uimg/b2c/newcatentries/0000000000-000000000140843141_2_800x800.jpg?ver=2024",
+            "goodsName": "苏泊尔(SUPOR) CYSB50YCW21QS-100 智能精控 球釜双胆电压力锅 5L 卡其色 ",
+            "price": "899",
+            "sale": "299"
+        }, {
+            "link": "http://product.suning.com/0000000000/147810618.html",
+            "img": "http://image3.suning.cn/uimg/b2c/newcatentries/0000000000-000000000147810618_2_800x800.jpg?ver=2006",
+            "goodsName": "德国双立人刀具套装8件套 厨房用品菜刀水果刀银点套刀组合不锈钢",
+            "price": "1999",
+            "sale": "1088"
+        }, {
+            "link": "http://product.suning.com/0000000000/139823854.html",
+            "img": "http://image1.suning.cn/uimg/b2c/newcatentries/0000000000-000000000139823854_2_800x800.jpg?ver=2009",
+            "goodsName": "美的微波炉 M1-211A 白色 21L 经典",
+            "price": "332",
+            "sale": "299"
+        }, {
+            "link": "http://product.suning.com/0000000000/126262323.html",
+            "img": "http://image1.suning.cn/uimg/b2c/newcatentries/0000000000-000000000126262323_2_800x800.jpg?ver=2004",
+            "goodsName": "苏泊尔（SUPOR）火红点2代无油烟炒锅PC32R2",
+            "price": "653",
+            "sale": "359"
+        }],
+        [{
+            "link": "http://product.suning.com/0000000000/128845468.html",
+            "img": "http://image1.suning.cn/uimg/b2c/newcatentries/0000000000-000000000128845468_3_800x800.jpg?ver=2015",
+            "goodsName": "康宝 高温二星级嵌入式 消毒碗柜家用",
+            "price": "1541",
+            "sale": "899"
+        }, {
+            "link": "http://product.suning.com/0000000000/153302625.html",
+            "img": "http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000153302625_2_800x800.jpg?ver=2054",
+            "goodsName": "沁园 RO-185(DT) 升级版 厨下式 净水机 直饮",
+            "price": "1998",
+            "sale": "998"
+        }, {
+            "link": "http://product.suning.com/0000000000/134774946.html",
+            "img": "http://image4.suning.cn/uimg/b2c/newcatentries/0000000000-000000000134774946_2_800x800.jpg?ver=2003",
+            "goodsName": "万家乐小厨宝D6-S001B 6升 上出水 电热水器",
+            "price": "599",
+            "sale": "299"
+        }, {
+            "link": "http://product.suning.com/0000000000/154564847.html",
+            "img": "http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000104746744_1_800x800.jpg?ver=2003",
+            "goodsName": "方太智能云魔方EMD3+FD21BE抽油烟机燃气灶烟灶套装",
+            "price": "6199",
+            "sale": "4190"
+        }, {
+            "link": "http://product.suning.com/0000000000/133827211.html",
+            "img": "http://image4.suning.cn/uimg/b2c/newcatentries/0000000000-000000000133827211_2_800x800.jpg?ver=2014",
+            "goodsName": "华帝 烟灶套餐 i11049油烟机+i10007B",
+            "price": "3999",
+            "sale": "1999"
+        }, {
+            "link": "http://product.suning.com/0000000000/121097204.html",
+            "img": "http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000121097204_1_800x800.jpg?ver=2015",
+            "goodsName": "老板吸油烟机CXW-200-8307 ",
+            "price": "4680",
+            "sale": "2580"
+        }],
+        [{
+            "link": "http://product.suning.com/0000000000/147863053.html",
+            "img": "http://image5.suning.cn/uimg/b2c/newcatentries/0000000000-000000000147863053_2_800x800.jpg?ver=2017",
+            "goodsName": "西门子484升多门冰箱",
+            "price": "7999",
+            "sale": "6999"
+        }, {
+            "link": "http://product.suning.com/0000000000/158992750.html",
+            "img": "http://image2.suning.cn/uimg/b2c/newcatentries/0000000000-000000000158992750_2_800x800.jpg?ver=2080",
+            "goodsName": "美的520升对开门风冷无霜电脑控温静音节能",
+            "price": "3699",
+            "sale": "2799"
+        }, {
+            "link": "http://product.suning.com/0000000000/172519947.html",
+            "img": "http://image5.suning.cn/uimg/b2c/newcatentries/0000000000-000000000172519947_2_800x800.jpg?ver=2027",
+            "goodsName": "美菱530升变频节能无霜保鲜对开门冰箱",
+            "price": "3999",
+            "sale": "2799"
+        }, {
+            "link": "http://product.suning.com/0000000000/102582434.html",
+            "img": "http://image3.suning.cn/uimg/b2c/newcatentries/0000000000-000000000102582434_2_800x800.jpg?ver=2020",
+            "goodsName": "三星545升对开门冰箱 风冷无霜 智能变频",
+            "price": "6691",
+            "sale": "3799"
+        }, {
+            "link": "http://product.suning.com/0000000000/123789209.html",
+            "img": "http://image5.suning.cn/uimg/b2c/newcatentries/0000000000-000000000123789209_2_800x800.jpg?ver=2016",
+            "goodsName": "容声 202升 三门爆款冰箱（银色）",
+            "price": "1465",
+            "sale": "1259"
+        }, {
+            "link": "http://product.suning.com/0000000000/124181891.html",
+            "img": "http://image4.suning.cn/uimg/b2c/newcatentries/0000000000-000000000124181891_2_800x800.jpg?ver=2010",
+            "goodsName": "海尔216升三门电脑智能控温家用冰箱 ",
+            "price": "2299",
+            "sale": "1469"
+        }]
+    ];
+
+$(function() {
+    //自定义楼层组件
+    Vue.component('my-component', {
+        template: '\
+			<div class="goodsBox">\
+				<div class="goodsPane" v-for="ele in floorData">\
+					<a :href="ele.link">\
+						<img :src="ele.img" class="goodsShow" />\
+						<p class="goodsName">{{ ele.goodsName }}</p>\
+						<div class="sale">\
+							￥{{ ele.sale }}\
+						</div>\
+					</a>\
+				</div>\
+			</div>\
+		',
+        props: ['floorData']
+    });
+    new Vue({
+        el: "#louBox",
+        data: {
+            louceng: louceng,
+            floorData: floorData
+        }
+    });
+});
